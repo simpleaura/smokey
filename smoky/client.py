9b9ecce162b0b4b5496bd32973aade3ce99955d9
Jonathan <jonathan.chen@sunrider.com>
2019-04-30 10:41:07 -0600

    added the client file

---
+++ smoky/client.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+    smoky.client
+    ~~~~~~~~~~~~
+
+    Smoky module that will setup the ORM and register the different objects.
+
+    :copyright: (c) 2019 by Simpleaura
+    :license: see LICENSE for more details
+
+"""
+
+from flask_sqlalchemy import SQLAlchemy
+
